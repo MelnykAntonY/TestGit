b8a1d28f9173aa13dd56e1e502d6f5b93f6d055b
anton.y.melnyk <github2017>
2017-07-11 16:52:00 +0300

    add varialeble

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,24 @@
  * Created by anton.y.melnyk on 7/10/2017.
  */
 public class Main {
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
     private int number = 5;
+
+    private int x =6;
+
+    private String name = "";
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
     private String test = "testgit";
     private double positionX = 0.013d;
 
@@ -13,6 +30,16 @@ public class Main {
     }
 
     private static void doSome(){
-        System.out.println(new Main().concat("Number", 0.0456));
+        System.out.println(new Main().concat("Number ", 0.0456));
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+
+        System.out.println(m.number);
+        Main.doSome();
+        m.setNumber(7);
+        System.out.println(m.getNumber());
+        System.out.println(new Main().number);
     }
 }
